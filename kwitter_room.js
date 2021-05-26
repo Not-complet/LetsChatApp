ba575b62f9a61a66cbf9b412decc3229d4b23aad
Not-complet <77133545+Not-complet@users.noreply.github.com>
2021-05-25 19:34:51 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBEE1rUi9ks4sCJVP2_bqgd2QRfWeBR7ZE",
+    authDomain: "kwitter-project-6dc41.firebaseapp.com",
+    projectId: "kwitter-project-6dc41",
+    storageBucket: "kwitter-project-6dc41.appspot.com",
+    messagingSenderId: "564708022900",
+    appId: "1:564708022900:web:fe1630cf8af1a4570d4f21"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
